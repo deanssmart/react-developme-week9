e3bd88d940c33c3bab5c434cee091f09f8057b9d
deanssmart <deanssmart@gmail.com>
2020-08-24 13:24:41 +0100

    Lorem Paragraph component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Header from "./components/Header";
+import Header from './components/Header';
+import Paragraph from './components/Paragraph'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Paragraph />
     </div>
   );
 }
