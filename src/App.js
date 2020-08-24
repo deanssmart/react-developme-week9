3b6637cdd84b351042be9a5ab3a57040b156684f
deanssmart <deanssmart@gmail.com>
2020-08-24 13:32:29 +0100

    Red Square component added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header';
-import Paragraph from './components/Paragraph'
+import Paragraph from './components/Paragraph';
+import Square from './components/Square';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Paragraph />
+      <Square />
     </div>
   );
 }
---
+++ src/components/Square.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Square = () => (
+    <div style={{ height: 200, width: 200, backgroundColor: "red"}}></div>
+);
+
+export default Square;
