ca14b64d036ca764869b238539c586a9e42255cf
deanssmart <deanssmart@gmail.com>
2020-08-24 16:54:42 +0100

    props added to square

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Square = () => (
-    <div style={{ height: 200, width: 200, backgroundColor: "red"}}></div>
+const Square = ({ colour }) => (
+    <div style={{ height: 200, width: 200, backgroundColor: colour }}></div>
 );
+
+Square.defaultProps = {
+    colour: "red",
+};
 
 export default Square;
