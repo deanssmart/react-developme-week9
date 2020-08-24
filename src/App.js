46328a6d43ef2b53fe63fd91801b595b5710c0f4
deanssmart <deanssmart@gmail.com>
2020-08-24 13:47:33 +0100

    People list component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header';
 import Paragraph from './components/Paragraph';
 import Square from './components/Square';
+import People from './components/People';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <Header />
       <Paragraph />
       <Square />
+      <People />
     </div>
   );
 }
