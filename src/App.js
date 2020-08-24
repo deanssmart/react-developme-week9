a1b7dd95c88e0c6b38640dfe0956e7c74a90e48d
deanssmart <deanssmart@gmail.com>
2020-08-24 13:53:13 +0100

    Stuff component added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import './App.css';
-
-import Header from './components/Header';
-import Paragraph from './components/Paragraph';
-import Square from './components/Square';
-import People from './components/People';
+import Stuff from './components/Stuff';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Paragraph />
-      <Square />
-      <People />
+      <Stuff />
     </div>
   );
 }
