6720fff3a372062c678f2de29bc3d9f928e2e20e
deanssmart <deanssmart@gmail.com>
2020-08-24 17:05:44 +0100

    props added to stuff to display square or not

--- src/components/Stuff.js
+++ src/components/Stuff.js
@@ -5,13 +5,17 @@ import Paragraph from './Paragraph';
 import Square from './Square';
 import People from './People';
 
-const Stuff = () => (
+const Stuff = ({ square }) => (
     <>
         <Header text = { "Hello, World" }/>
         <Paragraph message = { "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos." }/>
-        <Square colour = { "green" }/>
+        { square ? <Square colour = { "green" }/> : null }
         <People names = { ["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"] }/>
     </>
 );
+
+Stuff.defaultProps = {
+    square: true,
+};
 
 export default Stuff;
