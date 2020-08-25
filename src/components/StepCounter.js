616a78c489e31a8789a5cf8afed0fa24172b5426
deanssmart <deanssmart@gmail.com>
2020-08-25 15:33:53 +0100

    StepCounter component added

---
+++ src/components/StepCounter.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class StepCounter extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { counter: this.props.initial};
+
+        this.handleClickPos = this.handleClickPos.bind(this);
+        this.handleClickNeg = this.handleClickNeg.bind(this);
+    }
+
+    handleClickPos() {
+        if(this.state.counter < this.props.max){
+            let counter = this.state.counter + this.props.step;
+            this.setState({ counter: counter});
+        }            
+    }
+
+    handleClickNeg() {
+        if(this.state.counter > 0){
+            let counter = this.state.counter - this.props.step;
+            this.setState({ counter: counter});
+        }
+    }
+
+    render() {
+ 
+        const { counter } = this.state;
+
+        return (
+            <div className="card row">
+                <p className="text-center">{ counter }</p>
+                <button className="btn btn-success"  onClick={ this.handleClickPos }>+</button>
+                <button className="btn btn-danger"  onClick={ this.handleClickNeg }>-</button>
+            </div>
+        );
+    }
+}
+
+export default StepCounter;
--- src/components/Stuff.js
+++ src/components/Stuff.js
@@ -8,21 +8,23 @@ import Basket from '../tricksy-components/Basket';
 import Clicked from './Clicked';
 import ToggleText from './ToggleText';
 import Counter from './Counter';
+import StepCounter from './StepCounter';
 
 const Stuff = ({ square }) => (
     <>
         <Header>Hello, World</Header>
         <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos.</Paragraph>
-        { square ? <Square colour = { "hotpink" }/> : null }
-        <People names = { ["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"] }/>
-        <Basket items = {[
+        { square ? <Square colour={ "hotpink" }/> : null }
+        <People names={ ["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"] }/>
+        <Basket items={ [
                         { name: "Coffee", price: 2.10 },
                         { name: "Bananas", price: 3.50 },
                         { name: "Milk", price: 250.65 },
-                        { name: "The Great Milk Shortage by Simon Schama", price: 12.99 }]}/>
+                        { name: "The Great Milk Shortage by Simon Schama", price: 12.99 }] }/>
         <Clicked />
         <ToggleText initial="Hello" alternate="World"/>   
-        <Counter initial= { 50 } max= { 100 }/>                             
+        <Counter initial={ 50 } max={ 100 }/>        
+        <StepCounter initial={ 50 } max={ 100 } step={ 5 }/>                      
     </>
 );
 
