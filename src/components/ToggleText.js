485f888f3f87f80f56248824aa74ad2d3c3c3322
deanssmart <deanssmart@gmail.com>
2020-08-25 14:39:13 +0100

    Hello World Toggle component added

---
+++ src/components/ToggleText.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class ToggleText extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { toggle: false};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({ toggle: !this.state.toggle})
+    }
+
+    render() {
+        const { toggle } = this.state;
+        let text = this.state.toggle ? this.props.alternate : this.props.initial;
+
+        return (
+            <div className="card">
+                <p className="text-center">{ text }</p>
+                <button className="btn btn-success"  onClick={ this.handleClick }>Click me!</button>
+            </div>
+        );
+    }
+}
+
+export default ToggleText;
